5810a8409516a2949c9d295d711ce1139ec7140d
kun-zjk <zhujunkun10@126.com>
2021-10-27 23:12:39 +0800

    kun 1027

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,11 +1,9 @@
-import Vue, { createApp } from 'vue'
-import CircleMenu from 'vue-circle-menu'
+import  { createApp } from 'vue'
+// import CircleMenu from 'vue-circle-menu'
 import router from './router'
 import App from './App.vue'
 
 const app = createApp(App)
 app.use(router)
 app.mount('#app')
-
-Vue.component('CircleMenu', CircleMenu)
 
