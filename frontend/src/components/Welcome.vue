ed3983d32dc0c88af911d038c164bb8da6e73340
kun-zjk <zhujunkun10@126.com>
2021-10-29 11:42:03 +0800

    kk

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,20 +1,23 @@
 <template>
 <div>
-    <h1>eye that game</h1>
+    <h1>EYES THAT</h1>
     <van-image
     round
     width="10rem"
     height="10rem"
     :src="srcString"
   />
-  <img src="../assets/logo1.png"/>
+  <img class="tupian" src="../assets/logo1.png"/>
   
-    <h2>can you recogniza
+    <h2>Can you recognize
    which one is fake?</h2>
 
    <h3>let's try it now</h3>
 
-   <button v-on:click="hreftwo" class="test-one">Start</button>
+   <button v-on:click="hreftwo" class="test-one" style="
+   width:80px;
+   height:80px;
+   border-radius:50%">Start</button>
 
 </div>
 </template>
@@ -61,6 +64,7 @@ export default {
   color: #2c3e50;
   margin-top: 60px;
 }
+
 .test-one { 
   margin: 0 auto;
   
@@ -70,8 +74,12 @@ export default {
   padding: 1rem 0;
   border-radius: 4px;
   font-size: 1.6rem;
-  background: #435875;
-  
+  background: #172f50;
+ }
+
+ .tupian { 
+  height:300px;
+  width:500px;
  }
 
 </style>
