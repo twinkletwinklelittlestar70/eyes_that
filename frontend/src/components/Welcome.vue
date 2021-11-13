fdcddf68a2fe52daaa1d61e018333802a9259356
twinkletwinklelittlestar70 <linqianqian70@outlook.com>
2021-11-13 21:50:15 +0800

    feat: remove code unnessary

--- frontend/src/components/Welcome.vue
+++ frontend/src/components/Welcome.vue
@@ -1,59 +1,36 @@
 <template>
-<div class="welcome-page">
+  <div class="welcome-page">
     <h1>EYES THAT</h1>
-    <van-image
-    round
-    width="10rem"
-    height="10rem"
-    :src="srcString"
-  />
-  <img class="welcome-image" src="../assets/logo1.png"/>
-  
-    <h2>Can you recognize
-   which one is fake? Try it now!</h2>
+    <img class="welcome-image" src="../assets/logo1.png" />
 
-   <button v-on:click="hreftwo" class="primary-button" >Play against the AI</button>
-   <button v-on:click="routeToPicture" class="primary-button" >Challenge AI</button>
+    <h2>Can you recognize which one is fake? Try it now!</h2>
 
-</div>
+    <button v-on:click="hreftwo" class="primary-button">
+      Play against the AI
+    </button>
+    <button v-on:click="routeToPicture" class="primary-button">
+      Challenge AI
+    </button>
+  </div>
 </template>
 
 <script>
 import router from "../router/index.js";
-import {v4 as uuidv4} from 'uuid';
 export default {
-  name: 'App',
-  components: {
-    // HelloWorld
-  },
-  methods:{
-    hreftwo(){
-      console.log('testwhat')
-      let myuuid = uuidv4()
-      console.log(myuuid)
+  name: "App",
+  methods: {
+    hreftwo() {
       router.push({
-          path: '/game',
-          query: {
-            id: myuuid
-          }
-        })
+        path: "/game",
+      });
     },
     routeToPicture() {
       router.push({
-          path: '/picture'
-      })
-    }
+        path: "/picture",
+      });
+    },
   },
-  data(){
-    return {
-      myuuid:'',
-      // headimg: require("../assets/logo.png"),
-      srcString: "../assets/logo.png"
-    }     
-   }
-  
-  }
-
+};
 </script>
 
 <style>
@@ -64,7 +41,6 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
-  
 }
 .welcome-page {
   padding: 30px;
@@ -76,20 +52,19 @@ export default {
   border: none;
   color: #fff;
   width: 100%;
-  padding: .3em 0;
+  padding: 0.3em 0;
   border-radius: 4px;
   font-size: 1.2rem;
   background: #172f50;
 }
 
-.circle-button { 
-  width:80px;
-  height:80px;
-  border-radius:50%
- }
+.circle-button {
+  width: 80px;
+  height: 80px;
+  border-radius: 50%;
+}
 
- .welcome-image { 
+.welcome-image {
   width: 100%;
- }
-
+}
 </style>
